0219c71741ce53fcc200b1949e57d65bbe4e5e7a
MichaelKudron <kobrys87@gmail.com>
2024-05-22 23:09:27 +0200

    cos

---
+++ dist/angular-ang/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-X6EDR66C.mjs";import"./chunk-4N67AQOC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ dist/example/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UORQRO2F.mjs";import"./chunk-CKXNLLOH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
