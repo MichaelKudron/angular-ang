ccf3a409fd1494e0a8eefa858272f756948e027c
MichaelKudron <kobrys87@gmail.com>
2024-05-24 11:34:39 +0200

    Auto-generated commit

--- dist/angular-ang/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-X6EDR66C.mjs";import"./chunk-4N67AQOC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- dist/example/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-UORQRO2F.mjs";import"./chunk-CKXNLLOH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
