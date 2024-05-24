0680873217fcb3bca62331615eb934de497dfa07
MichaelKudron <kobrys87@gmail.com>
2024-05-24 08:06:43 +0200

    hee

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, booleanAttribute } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+import { NavabrComponent } from '../components/navabr/navabr.component';
+import { NgIf } from '@angular/common';
+import { MenuComponent } from '../components/menu/menu.component';
+import { NavbarmenuComponent } from '../components/navbarmenu/navbarmenu.component';
+import { MenuhandlingdataService } from './menuhandlingdata.service';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule,NavabrComponent,NgIf,MenuComponent,NavbarmenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  constructor(private  menuhandlingdataServic:MenuhandlingdataService){}
+  isClicked=false;
+  ngOnInit():void{
+    
+    this.menuhandlingdataServic.isMenuClicked$.subscribe(value=>{this.isClicked=value})
+  }
+  title = 'angProj';
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { AboutComponent } from '../components/about/about.component';
 import { OurteamComponent } from '../components/ourteam/ourteam.component';
+import { ExpertsComponent } from '../components/experts/experts.component';
 const routes: Routes = [
     {
         path:'',
@@ -17,6 +18,11 @@ const routes: Routes = [
         path:'ourteam',
         component:OurteamComponent,
         title:'Our Team'
+    },
+    {
+        path:'experts',
+        component:ExpertsComponent,
+        title:'Experts'
     }
 ]
 export default routes;
